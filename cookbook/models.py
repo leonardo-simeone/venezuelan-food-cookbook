a2cd1f1a066697e5b056f7d4f99c684f6735e37b
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-21 16:54:49 +0000

    Create recipe model

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Recipe(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_creator', null=True, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    short_description = models.CharField(max_length=150, null=True, blank=True)
+    instructions = models.TextField(null=True, blank=True)
+    food_image = CloudinaryField('image', default='default-image')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title + ' by ' + self.creator.username
