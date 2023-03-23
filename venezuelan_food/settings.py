b706fb0166ae76f29cfffd3a6025c77522177a5f
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-23 20:13:32 +0000

    Update site id

--- venezuelan_food/settings.py
+++ venezuelan_food/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'cookbook',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
