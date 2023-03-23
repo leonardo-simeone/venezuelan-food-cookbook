b705d85ff6dc62b2c313872df55d0d2ee54e22ca
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-23 15:09:33 +0000

    Create recipe unit view

--- cookbook/views.py
+++ cookbook/views.py
@@ -9,3 +9,13 @@ def recipes(request):
     context = {'recipes_list': recipes_list}
 
     return render(request, 'index.html', context)
+
+
+def recipeUnit(request, pk):
+
+    recipe_object = Recipe.objects.get(id=pk)
+    comments = recipe_object.comments.all()
+
+    context = {'recipe_object': recipe_object, 'comments': comments}
+
+    return render(request, 'recipe.html', context)
