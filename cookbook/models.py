f303a55d52433cdc45521fc0a848f20696209a1a
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-24 16:24:53 +0000

    Create ingredients field in recipe model

--- cookbook/models.py
+++ cookbook/models.py
@@ -7,7 +7,7 @@ class Recipe(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_creator', null=True, blank=True)
     title = models.CharField(max_length=200, null=True, blank=True, unique=True)
     short_description = models.CharField(max_length=150, null=True, blank=True)
-    instructions = models.TextField(null=True, blank=True)
+    ingredients = models.TextField()
     food_image = CloudinaryField('image', default='default-image')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
