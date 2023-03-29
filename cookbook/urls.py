ab60f38dfbefc63826faa44db6e4bb605cdc1ef4
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-29 18:25:02 +0000

    Connect update & delete recipe views to app urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.recipes, name='recipes'),
-    path('recipe/<int:pk>/', views.recipeUnit, name='recipeunit'),
+    path('recipe/<str:pk>/', views.recipeUnit, name='recipeunit'),
     path('register/', views.registerView, name='register'),
     path('login/', views.loginView, name='login'),
     path('logout/', views.logoutView, name='logout'),
     path('create-recipe/', views.createRecipe, name='create-recipe'),
+    path('update-recipe/<str:pk>/', views.updateRecipe, name='update-recipe'),
+    path('delete-recipe/<str:pk>/', views.deleteRecipe, name='delete-recipe'),
 ]
