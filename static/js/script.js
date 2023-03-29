b5af1506dd6e1170201ef7dee5a374b314e917f1
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-29 18:48:07 +0000

    Add js code to add classes to update form fields

---
+++ static/js/script.js
@@ -0,0 +1,39 @@
+var title = document.getElementById('id_title');
+
+title.className += ' fadeIn first';
+// -------------------------------------------------------------------
+var shortDescription = document.getElementById('id_short_description');
+
+shortDescription.className += ' fadeIn second';
+// -------------------------------------------------------------------
+var ingredients = document.getElementById('id_ingredients');
+
+ingredients.className += ' fadeIn third';
+// -------------------------------------------------------------------
+var instructions = document.getElementById('id_instructions');
+
+instructions.className += ' fadeIn fourth';
+// -------------------------------------------------------------------
+var foodImage = document.getElementById('id_food_image');
+
+foodImage.className += ' fadeIn fifth';
+// -------------------------------------------------------------------
+var labelOne = document.getElementsByTagName('label')[0];
+
+labelOne.className += ' fadeIn first';
+// -------------------------------------------------------------------
+var labelTwo = document.getElementsByTagName('label')[1];
+
+labelTwo.className += ' fadeIn second';
+// -------------------------------------------------------------------
+var labelThird = document.getElementsByTagName('label')[2];
+
+labelThird.className += ' fadeIn third';
+// -------------------------------------------------------------------
+var labelFourth = document.getElementsByTagName('label')[3];
+
+labelFourth.className += ' fadeIn fourth';
+// -------------------------------------------------------------------
+var labelFifth = document.getElementsByTagName('label')[4];
+
+labelFifth.className += ' fadeIn fifth';
