846aef9a365f41f95401bee4f66ee5fb83790684
Leonardo Simeone <leo_sime@yahoo.com>
2023-03-29 18:20:22 +0000

    Create the comment form model

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Recipe
+from .models import Recipe, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -15,3 +15,9 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
