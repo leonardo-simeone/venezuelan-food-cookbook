8aa6ae129fe383c90bfa29ea199eaa50dc7b89ab
Leonardo Simeone <leo_sime@yahoo.com>
2023-04-12 15:53:59 +0000

    Delete unnecessary code in js file

--- static/js/script.js
+++ static/js/script.js
@@ -58,28 +58,3 @@ foodTags.classList.add('fadeIn', 'fifth');
 var foodImage = document.getElementById('id_food_image');
 
 foodImage.classList.add('fadeIn', 'sixth');
-// -------------------------------------------------------------------
-var labelOne = document.getElementsByTagName('label')[0];
-
-labelOne.classList.add('fadeIn', 'first');
-// -------------------------------------------------------------------
-var labelTwo = document.getElementsByTagName('label')[1];
-
-labelTwo.classList.add('fadeIn', 'second');
-// -------------------------------------------------------------------
-var labelThird = document.getElementsByTagName('label')[2];
-
-labelThird.classList.add('fadeIn', 'third');
-// -------------------------------------------------------------------
-var labelFourth = document.getElementsByTagName('label')[3];
-
-labelFourth.classList.add('fadeIn', 'fourth');
-// -------------------------------------------------------------------
-var labelFifth = document.getElementsByTagName('label')[4];
-
-labelFifth.classList.add('fadeIn', 'fifth');
-// -------------------------------------------------------------------
-var labelSixth = document.getElementsByTagName('label')[5];
-
-labelSixth.classList.add('fadeIn', 'sixth');
-
