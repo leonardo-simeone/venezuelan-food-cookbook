92a07b7f3e01ac26f319f45bcbceabf1e1fccef1
Leonardo Simeone <leo_sime@yahoo.com>
2023-04-14 11:58:44 +0000

    Add tinymce widget to instructions field in admin

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
+from tinymce.widgets import TinyMCE
 
-admin.site.register(Recipe)
+
+class CookbookAdmin(admin.ModelAdmin):
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'instructions':
+            kwargs['widget'] = TinyMCE()
+        return super(CookbookAdmin, self).formfield_for_dbfield(db_field,**kwargs)
+
+
+admin.site.register(Recipe, CookbookAdmin)
 admin.site.register(Comment)
