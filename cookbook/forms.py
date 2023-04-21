dd17865d736df80c5e0444242729b41e5c1fbfd1
Leonardo Simeone <leo_sime@yahoo.com>
2023-04-21 15:33:27 +0000

    Add docstrings to forms classes

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -16,6 +16,15 @@ RECIPE_TAGS = (
 
 
 class RecipeForm(ModelForm):
+
+    """
+    The RecipeForm class inherits from django.forms ModelForm,
+    it contains a variable that defines the form tags field
+    which will use RECIPE_TAGS as its choices and CheckboxSelectMultiple
+    as its widget. It also has a meta class to set the model and
+    fields to use in the form.
+    """
+
     tags = forms.MultipleChoiceField(
         required=False,
         choices=RECIPE_TAGS,
@@ -28,12 +37,28 @@ class RecipeForm(ModelForm):
 
 
 class NewUserForm(UserCreationForm):
+
+    """
+    The NewUserForm class inherits from
+    django.contrib.auth.forms UserCreationForm,
+    it contains a meta class to set the model
+    and fields to use in the form.
+    """
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 
 class CommentForm(forms.ModelForm):
+
+    """
+    The CommentForm class inherits from
+    django.forms ModelForm, it contains
+    a meta class to set the model and fields
+    to use in the form.
+    """
+
     class Meta:
         model = Comment
         fields = ['body']
