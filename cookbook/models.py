b59627771302edd748344d737de5f454b0bd8ecf
Leonardo Simeone <leo_sime@yahoo.com>
2023-04-21 15:34:42 +0000

    Add docstrings to models classes

--- cookbook/models.py
+++ cookbook/models.py
@@ -7,6 +7,20 @@ from multiselectfield import MultiSelectField
 
 class Recipe(models.Model):
 
+    """
+    The Recipe class inherits from django.db models, it contains a tuple
+    which defines the recipe tags used in the tags field. It also has all
+    the fields used for the model. This model is used to hold the information
+    pertaining to recipe objects attributes. There are two particular
+    relationships in the model, the creator field has a One to Many
+    relationship with User since one user can create many recipes and
+    the likes field has a M2M relationship with User since many users
+    can like many recipes. This model has a meta class to determine the
+    descending order of the recipe objects and two helper methods,
+    one to count the number of likes and one to represent the objects
+    with a custom string.
+    """
+
     RECIPE_TAGS = (('Breakfast', 'Breakfast'), ('Lunch', 'Lunch'), ('Dinner', 'Dinner'), ('Supper', 'Supper'), ('Dessert', 'Dessert'), ('Drink', 'Drink'),)
 
     creator = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name='recipe_creator')
@@ -22,7 +36,7 @@ class Recipe(models.Model):
 
     class Meta:
         ordering = ['-created']
-    
+
     def total_likes(self):
         return self.likes.count()
 
@@ -31,6 +45,17 @@ class Recipe(models.Model):
 
 
 class Comment(models.Model):
+
+    """
+    The Comment class inherits from django.db models, it contains
+    the information pertaining to comment objects attributes.
+    There is one particular relationship in the model, the recipe field
+    has a One to Many relationship with Recipe since one recipe
+    can have many comments. This model has a meta class to determine
+    the ascending order of the comment objects and one helper method
+    to represent the objects with a custom string.
+    """
+
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True, blank=True, related_name='comments')
     name = models.CharField(max_length=200)
     body = models.TextField()
