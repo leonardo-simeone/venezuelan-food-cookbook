d79ea0a88f045971bf865dcdc4db088076975c39
Leonardo Simeone <leo_sime@yahoo.com>
2023-05-03 13:18:06 +0000

    Refactor test models

--- cookbook/test_models.py
+++ cookbook/test_models.py
@@ -8,11 +8,14 @@ class TestRecipeModel(TestCase):
 
     """
     The setUp method is used to create a User object and a Recipe object.
-    The TestRecipeModel class has three test methods: test_recipe_creation, test_recipe_tags, and test_recipe_likes.
-    - In test_recipe_creation, we check if a Recipe object is created correctly and if all its fields are correctly saved.
-    - In test_recipe_tags, we test if we can set and retrieve the tags for a Recipe.
-    - In test_recipe_likes, we test if we can add likes to a Recipe, retrieve the number of likes,
-    and check if a user is in the likes queryset.
+    The TestRecipeModel class has three test methods: test_recipe_creation,
+    test_recipe_tags, and test_recipe_likes.
+    - In test_recipe_creation, we check if a Recipe object
+    is created correctly and if all its fields are correctly saved.
+    - In test_recipe_tags, we test if we can set and retrieve
+    the tags for a Recipe.
+    - In test_recipe_likes, we test if we can add likes to a Recipe,
+    retrieve the number of likes, and check if a user is in the likes queryset.
     """
 
     # Creates the necessary user and recipe objects to run tests.
@@ -34,8 +37,12 @@ class TestRecipeModel(TestCase):
     def test_recipe_creation(self):
         self.assertEqual(str(self.recipe), 'Test Recipe by testuser')
         self.assertEqual(self.recipe.creator, self.user)
-        self.assertEqual(self.recipe.short_description, 'This is a test recipe')
-        self.assertEqual(self.recipe.ingredients, 'Test ingredient 1, Test ingredient 2')
+        self.assertEqual(
+            self.recipe.short_description, 'This is a test recipe'
+            )
+        self.assertEqual(
+            self.recipe.ingredients, 'Test ingredient 1, Test ingredient 2'
+            )
         self.assertEqual(self.recipe.instructions, '<p>Test instruction</p>')
         self.assertEqual(self.recipe.tags, None)
         self.assertEqual(self.recipe.total_likes(), 0)
@@ -67,12 +74,17 @@ class TestRecipeModel(TestCase):
 class TestCommentModel(TestCase):
 
     """
-    The TestCommentModel class has two test methods: test_comment_name and test_comment_ordering.
-    The setUpTestData class method is used to set up the test data. In this case, we create a User object and a Recipe object,
+    The TestCommentModel class has two test methods: test_comment_name
+    and test_comment_ordering.
+    The setUpTestData class method is used to set up the test data.
+    In this case, we create a User object and a Recipe object,
     and then create a Comment object that is associated with the Recipe object.
-    - test_comment_name tests that the __str__ method of the Comment model returns the expected string.
-    - test_comment_ordering tests that the Meta.ordering attribute of the Comment model is working correctly
-    by creating a second Comment object and checking that the two objects are ordered correctly when retrieved from the database.
+    - test_comment_name tests that the __str__ method
+    of the Comment model returns the expected string.
+    - test_comment_ordering tests that the Meta.ordering attribute
+    of the Comment model is working correctly by creating a second
+    Comment object and checking that the two objects are ordered correctly
+    when retrieved from the database.
     """
 
     @classmethod
@@ -89,15 +101,19 @@ class TestCommentModel(TestCase):
             updated=timezone.now(),
         )
 
-    # Tests that the __str__ method of the Comment model returns the expected string.
+    # Tests that the __str__ method of the Comment model
+    # returns the expected string.
     def test_comment_name(self):
         comment = Comment.objects.get(id=1)
-        expected_name = f'Comment made by {comment.name} on {comment.recipe.title}'
+        expected_name = (
+            f'Comment made by {comment.name} on {comment.recipe.title}'
+            )
         self.assertEqual(expected_name, str(comment))
 
-    # Tests that the Meta.ordering attribute of the Comment model is working correctly
-    # by creating a second Comment object and checking that the two objects are ordered
-    # correctly when retrieved from the database.
+    # Tests that the Meta.ordering attribute of the Comment model
+    # is working correctly by creating a second Comment object
+    # and checking that the two objects are ordered correctly
+    # when retrieved from the database.
     def test_comment_ordering(self):
         comment1 = Comment.objects.get(id=1)
         comment2 = Comment.objects.create(
