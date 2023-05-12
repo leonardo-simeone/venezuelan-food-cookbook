c8a3a665b82d43a8ae6c49a8672cd85b881c74d6
Leonardo Simeone <leo_sime@yahoo.com>
2023-05-12 19:15:03 +0000

    Connect contact view to cookbook urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('about-us/', views.aboutUs, name='about-us'),
     path('gallery/', views.gallery, name='gallery'),
     path('like-recipe/<str:pk>/', views.likeRecipe, name='like-recipe'),
+    path('contact/', views.contact, name='contact'),
 ]
